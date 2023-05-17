4f43c740a8a564ee6bc69cb571488793e9e490c1
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-05-17 17:12:25 -0300

    REQ4 - Finalizado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const crypto = require('crypto');
-const { v4: uuidv4 } = require('uuid');
 const { readTalker } = require('./utils/fsUtils');
 
 const app = express();
@@ -35,7 +34,27 @@ app.get('/talker/:id', async (req, res) => {
   return res.status(200).json(talkers.find((e) => e.id === Number(id)));
 });
 
-app.post('/login', async (req, res) => {
+// REQ 03 - 04
+const validateEmail = (req, res, next) => {
+  const { email } = req.body;
+  const regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+  const emailError = 'O "email" deve ter o formato "email@email.com"';
+  const emailObrigatoryError = 'O campo "email" é obrigatório';
+  if (!email) return res.status(400).json({ message: emailObrigatoryError });
+  if (!email.match(regex)) return res.status(400).json({ message: emailError });
+  next();
+};
+
+const validatePassword = (req, res, next) => {
+  const { password } = req.body;
+  const passwordObrigatoryError = 'O campo "password" é obrigatório';
+  const passwordMinLenthError = 'O "password" deve ter pelo menos 6 caracteres';
+  if (!password) return res.status(400).json({ message: passwordObrigatoryError });
+  if (password.length < 6) return res.status(400).json({ message: passwordMinLenthError });
+  next();
+};
+
+app.post('/login', validateEmail, validatePassword, async (req, res) => {
   const token = crypto.randomBytes(8).toString('hex');
   return res.status(200).json({ token });
 });
