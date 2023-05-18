19fe845cbdde9c0fa105c38e756b128884dd0934
Gabriel <gabrielkelvinfreitas@gmail.com>
2023-05-18 10:56:48 -0300

    REQ05 - Finalizado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const crypto = require('crypto');
-const { readTalker } = require('./utils/fsUtils');
+const { readTalker, writeTalker } = require('./utils/fsUtils');
 
 const app = express();
 app.use(express.json());
@@ -57,4 +57,71 @@ const validatePassword = (req, res, next) => {
 app.post('/login', validateEmail, validatePassword, async (req, res) => {
   const token = crypto.randomBytes(8).toString('hex');
   return res.status(200).json({ token });
+});
+
+// REQ 05
+
+const validateToken = (req, res, next) => {
+  const token = req.header('Authorization');
+  console.log(token);
+  if (!token) return res.status(401).json({ message: 'Token não encontrado' });
+  if (token.length !== 16 || typeof (token) !== 'string') {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+  next();
+};
+
+const validateName = (req, res, next) => {
+  const { name } = req.body;
+  const obrigatoryNameError = 'O campo "name" é obrigatório';
+  if (!name) return res.status(400).json({ message: obrigatoryNameError });
+  const minLengthNameError = 'O "name" deve ter pelo menos 3 caracteres';
+  if (name.length < 3) return res.status(400).json({ message: minLengthNameError });
+  next();
+};
+
+const validateAge = (req, res, next) => {
+  const { age } = req.body;
+  const obrigatoryAgeError = 'O campo "age" é obrigatório';
+  if (!age) return res.status(400).json({ message: obrigatoryAgeError });
+  const validate = Number.isInteger(age) && age >= 18;
+  const minAgeError = 'O campo "age" deve ser um número inteiro igual ou maior que 18';
+  if (!validate) return res.status(400).json({ message: minAgeError });
+  next();
+};
+
+const validateNullTalk = (req, res, next) => {
+  const { talk } = req.body;
+  const obrigatoryTalkError = 'O campo "talk" é obrigatório';
+  if (!talk) return res.status(400).json({ message: obrigatoryTalkError });
+  const { watchedAt, rate } = talk;
+  const obrigatoryWatchedAtError = 'O campo "watchedAt" é obrigatório';
+  if (!watchedAt) return res.status(400).json({ message: obrigatoryWatchedAtError });
+  const obrigatoryRateError = 'O campo "rate" é obrigatório';
+  if (rate === null || rate === undefined) {
+    return res.status(400).json({ message: obrigatoryRateError });
+  }
+
+  next(); 
+};
+
+const validateFormatTalk = (req, res, next) => {
+  const { talk } = req.body;
+  const { watchedAt, rate } = talk;
+  const regexDate = /^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/;
+  const dateFormatError = 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"';
+  if (!watchedAt.match(regexDate)) return res.status(400).json({ message: dateFormatError });
+  const validateRate = Number.isInteger(rate) && rate >= 1 && rate <= 5;
+  console.log(validateRate);
+  const rateFormatError = 'O campo "rate" deve ser um número inteiro entre 1 e 5';
+  if (!validateRate) return res.status(400).json({ message: rateFormatError });
+  next();
+};
+
+app.post('/talker', validateToken, validateName, validateAge, validateNullTalk, 
+validateFormatTalk, async (req, res) => {
+  const newTalker = req.body;
+  const newArq = await writeTalker(newTalker);
+  const lastTalker = newArq[newArq.length - 1];
+  return res.status(201).json(lastTalker);
 });
--- src/utils/fsUtils.js
+++ src/utils/fsUtils.js
@@ -13,7 +13,22 @@ async function readTalker() {
     console.log(`Erro no arquivo ${err}`);
   }
 }
-readTalker();
+
+async function writeTalker(newTalker) {
+  try {
+    const oldFile = await readTalker();
+    const lastId = oldFile[oldFile.length - 1].id;
+    const newTalkerWithId = { id: lastId + 1, ...newTalker };
+    const newFile = JSON.stringify([...oldFile, newTalkerWithId]);
+    await fs.writeFileSync(path.resolve(__dirname, talkerFile), newFile);
+
+    return JSON.parse(newFile);
+  } catch (err) {
+    console.log(`Erro no arquivo: ${err}`);
+  }
+}
+
 module.exports = {
   readTalker,
+  writeTalker,
 };
